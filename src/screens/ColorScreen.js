370157789d62d70ffc49e44c8cbedf039bb6767b
brendino500 <brenda.ty@live.com>
2020-12-18 16:20:44 +0000

    lecture 45

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet, Button } from 'react-native'
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([])
   return (
     <View>
-      <Button title="Add a Color" />
-      <View style={{ height: 100, width: 100, backgroundColor: randomRbg() }} />
+      <Button title="Add a Color" onPress={() => {
+        setColors([...colors, randomRgb()])
+      }}/>
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   )
 }
 
-const randomRbg = () => {
+const randomRgb = () => {
   const red = Math.floor(Math.random() * 256)
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
