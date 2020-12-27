4a2f9923d4d43a1ec96e5a47ba19d710eeaa7f3a
brendino500 <brenda.ty@live.com>
2020-12-27 10:43:33 +0000

    lecture 67 - conditional rendering

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [password, setPassword] = useState("");
+
   return (
     <View>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={password}
+        onChangeText={(e) => setPassword(e)}
       />
+      {password.length < 4 ? (
+        <Text>Password must be at least 4 characters</Text>
+      ) : null}
     </View>
   );
 };
